caceda3bffd7e81511742bd332e1d7460f011a9f
[HobumPark] <[gloryofalive@naver.com]>
2022-12-10 14:52:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const getData=async()=>{
+    const result=await axios.get('/board')
+    console.log(result)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={getData}>요청</button>
     </div>
   );
 }
